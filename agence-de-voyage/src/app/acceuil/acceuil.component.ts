f498264429f258933f5f2ab4d903dcede19888e0
DorraBM <benmimounkarima77@gmail.com>
2021-12-03 13:10:42 +0100

    hello

--- agence-de-voyage/src/app/acceuil/acceuil.component.ts
+++ agence-de-voyage/src/app/acceuil/acceuil.component.ts
@@ -3,83 +3,39 @@ import { Hotel } from '../../model/hotel';
 import { HotelsService } from '../../service/hotels.service';
 import { NgbRatingConfig } from '@ng-bootstrap/ng-bootstrap';
 import { AuthService } from 'src/service/auth.service';
-<<<<<<< HEAD
 import { ActivatedRoute, Router } from '@angular/router';
-=======
-import { ActivatedRoute } from '@angular/router';
 import {MatSnackBar} from '@angular/material/snack-bar';
->>>>>>> d436fb86a517967b5c2e4609089f6f1a9161fce6
 @Component({
   selector: 'app-acceuil',
   templateUrl: './acceuil.component.html',
   styleUrls: ['./acceuil.component.css']
 })
 export class AcceuilComponent implements OnInit {
-<<<<<<< HEAD
 
   listHotel: Hotel[] = [];
   allHotels: Hotel[] = [];
   rating = 0;
-  constructor(private hotelService: HotelsService, config: NgbRatingConfig, public authService: AuthService, private route: ActivatedRoute, private router: Router) {
+  /*constructor(private hotelService: HotelsService, config: NgbRatingConfig, public authService: AuthService, private route: ActivatedRoute, private router: Router) {
     config.max = 5;
     config.readonly = true;
  
-  }
-  getNavigation(link, id) {
-    if (id === '') {
-      this.router.navigate([link]);
-    } else {
-      this.router.navigate([link + '/' + id]);
-    }
-  }
+  }*/
+ 
 
-
-  ngOnInit(): void {
-    this.hotelService.getHotels().subscribe(data => this.listHotel = data);
-    this.hotelService.getHotels().subscribe(data => this.allHotels = data);
-
-
-
-
-
-  }
-  onSupprimer(id: number) {
-    this.hotelService.supprimerHotel(id).subscribe(data => {
-      this.listHotel = this.listHotel.filter(elet => elet.id != id);
-    });
-  }
-  onModifier(id: number) {
-    //this.hotelService.modifierHotel(id,this.listHotel).subscribe(data=>console.log(data));
-  }
-  onPersister() {
-    // this.hotelService.persister();
-  }
-
-  search(a: string) {
-
-    if (a != "") {
-
-      this.listHotel = this.allHotels.filter(hotel =>
-        hotel.lieu.toLowerCase().includes(a.toLowerCase()));
-
-    }
-    else
-     this.listHotel=this.allHotels;
-
-
-  }
-=======
   newHotel= new Hotel(10, '', '', '', 0,0,true);
-  listHotel:Hotel[]=[];
-  rating=0;
-   constructor( private hotelService:HotelsService,private _snackBar: MatSnackBar, config: NgbRatingConfig,public authService: AuthService,private route: ActivatedRoute)
+  /*listHotel:Hotel[]=[];
+  rating=0;*/
+   constructor( private hotelService:HotelsService,private _snackBar: MatSnackBar, config: NgbRatingConfig,public authService: AuthService,private route: ActivatedRoute,private router: Router)
     {config.max = 5;
      config.readonly = true;
     hotelService.getHotels().subscribe(data=>this.listHotel=data);
     }
  
    ngOnInit(): void {
-    this.hotelService.getHotels().subscribe(data=>this.listHotel=data);
+    
+    this.hotelService.getHotels().subscribe(data => this.listHotel = data);
+    this.hotelService.getHotels().subscribe(data => this.allHotels = data);
+
    
     //   this.route.params.subscribe(params => {
     //      if (params.searchTerm)
@@ -90,6 +46,13 @@ export class AcceuilComponent implements OnInit {
     //  })
     
    }
+   getNavigation(link, id) {
+    if (id === '') {
+      this.router.navigate([link]);
+    } else {
+      this.router.navigate([link + '/' + id]);
+    }
+  }
    onSupprimer(id:number)
    {
      this.hotelService.supprimerHotel(id).subscribe(data =>{
@@ -112,7 +75,19 @@ export class AcceuilComponent implements OnInit {
   /*ErrorSnackBar(message: string) {
     this._snackBar.open(message, 'ERROR', { duration: 3000 });
   }*/
+  search(a: string) {
+
+    if (a != "") {
+
+      this.listHotel = this.allHotels.filter(hotel =>
+        hotel.lieu.toLowerCase().includes(a.toLowerCase()));
+
+    }
+    else
+     this.listHotel=this.allHotels;
+
+
+  }
   
->>>>>>> d436fb86a517967b5c2e4609089f6f1a9161fce6
 
 }
