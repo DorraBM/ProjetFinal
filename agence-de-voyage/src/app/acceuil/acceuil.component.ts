b08d3ae1001357b22a5e891db04fd27d9c857171
DorraBM <benmimounkarima77@gmail.com>
2021-12-03 01:42:23 +0100

    03/12

--- agence-de-voyage/src/app/acceuil/acceuil.component.ts
+++ agence-de-voyage/src/app/acceuil/acceuil.component.ts
@@ -4,16 +4,17 @@ import { HotelsService } from '../../service/hotels.service';
 import { NgbRatingConfig } from '@ng-bootstrap/ng-bootstrap';
 import { AuthService } from 'src/service/auth.service';
 import { ActivatedRoute } from '@angular/router';
+import {MatSnackBar} from '@angular/material/snack-bar';
 @Component({
   selector: 'app-acceuil',
   templateUrl: './acceuil.component.html',
   styleUrls: ['./acceuil.component.css']
 })
 export class AcceuilComponent implements OnInit {
-
+  newHotel= new Hotel(10, '', '', '', 0,0,true);
   listHotel:Hotel[]=[];
   rating=0;
-   constructor( private hotelService:HotelsService, config: NgbRatingConfig,public authService: AuthService,private route: ActivatedRoute)
+   constructor( private hotelService:HotelsService,private _snackBar: MatSnackBar, config: NgbRatingConfig,public authService: AuthService,private route: ActivatedRoute)
     {config.max = 5;
      config.readonly = true;
     hotelService.getHotels().subscribe(data=>this.listHotel=data);
@@ -37,15 +38,22 @@ export class AcceuilComponent implements OnInit {
      this.listHotel=this.listHotel.filter(elet=>elet.id!=id);
      });  
    }
-   onModifier(id:number)
-   {
-     //this.hotelService.modifierHotel(id,this.listHotel).subscribe(data=>console.log(data));
+   onModifier(t:Hotel)
+   { this.newHotel=t;
+     
    }
-   onPersister()
+   upDate()
    {
-     // this.hotelService.persister();
+    this.hotelService.modifierHotel(this.newHotel.id,this.newHotel).subscribe(data=>
+      {this.SuccessSnackBar("Hotel modified");}
+      );
    }
-
+   SuccessSnackBar(message: string) {
+    this._snackBar.open(message, 'SUCCEEDED', { duration: 3000});
+  }
+  /*ErrorSnackBar(message: string) {
+    this._snackBar.open(message, 'ERROR', { duration: 3000 });
+  }*/
   
 
 }
