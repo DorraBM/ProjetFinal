569cfacf6ca41bed1818d76a2d053aea305a9b80
nour jandoubi <jandoubi.nourelhouda@gmail.com>
2021-12-03 13:12:57 +0100

    hi

--- agence-de-voyage/src/app/acceuil/acceuil.component.ts
+++ agence-de-voyage/src/app/acceuil/acceuil.component.ts
@@ -3,24 +3,20 @@ import { Hotel } from '../../model/hotel';
 import { HotelsService } from '../../service/hotels.service';
 import { NgbRatingConfig } from '@ng-bootstrap/ng-bootstrap';
 import { AuthService } from 'src/service/auth.service';
-<<<<<<< HEAD
 import { ActivatedRoute, Router } from '@angular/router';
-=======
-import { ActivatedRoute } from '@angular/router';
+
 import {MatSnackBar} from '@angular/material/snack-bar';
->>>>>>> d436fb86a517967b5c2e4609089f6f1a9161fce6
 @Component({
   selector: 'app-acceuil',
   templateUrl: './acceuil.component.html',
   styleUrls: ['./acceuil.component.css']
 })
 export class AcceuilComponent implements OnInit {
-<<<<<<< HEAD
-
+  newHotel= new Hotel(10, '', '', '', 0,0,true);
   listHotel: Hotel[] = [];
   allHotels: Hotel[] = [];
   rating = 0;
-  constructor(private hotelService: HotelsService, config: NgbRatingConfig, public authService: AuthService, private route: ActivatedRoute, private router: Router) {
+  constructor(private hotelService: HotelsService, config: NgbRatingConfig,private _snackBar: MatSnackBar, public authService: AuthService, private route: ActivatedRoute, private router: Router) {
     config.max = 5;
     config.readonly = true;
  
@@ -48,12 +44,8 @@ export class AcceuilComponent implements OnInit {
       this.listHotel = this.listHotel.filter(elet => elet.id != id);
     });
   }
-  onModifier(id: number) {
-    //this.hotelService.modifierHotel(id,this.listHotel).subscribe(data=>console.log(data));
-  }
-  onPersister() {
-    // this.hotelService.persister();
-  }
+  
+  
 
   search(a: string) {
 
@@ -68,34 +60,8 @@ export class AcceuilComponent implements OnInit {
 
 
   }
-=======
-  newHotel= new Hotel(10, '', '', '', 0,0,true);
-  listHotel:Hotel[]=[];
-  rating=0;
-   constructor( private hotelService:HotelsService,private _snackBar: MatSnackBar, config: NgbRatingConfig,public authService: AuthService,private route: ActivatedRoute)
-    {config.max = 5;
-     config.readonly = true;
-    hotelService.getHotels().subscribe(data=>this.listHotel=data);
-    }
  
-   ngOnInit(): void {
-    this.hotelService.getHotels().subscribe(data=>this.listHotel=data);
-   
-    //   this.route.params.subscribe(params => {
-    //      if (params.searchTerm)
-    //       this.listHotel = this.hotelService.getHotels().filter(hotel =>
-    //         hotel.lieu.toLowerCase().includes(params.searchTerm.toLowerCase()));
-    //    else
-    //       this.hotelService.getHotels();
-    //  })
-    
-   }
-   onSupprimer(id:number)
-   {
-     this.hotelService.supprimerHotel(id).subscribe(data =>{
-     this.listHotel=this.listHotel.filter(elet=>elet.id!=id);
-     });  
-   }
+  
    onModifier(t:Hotel)
    { this.newHotel=t;
      
@@ -113,6 +79,5 @@ export class AcceuilComponent implements OnInit {
     this._snackBar.open(message, 'ERROR', { duration: 3000 });
   }*/
   
->>>>>>> d436fb86a517967b5c2e4609089f6f1a9161fce6
 
 }
